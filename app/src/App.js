66fed1038e26a1044f061f4f982be2ca51e5f666
Justin Peczenij <justinpeczenij@gmail.com>
2021-04-14 16:22:06 -0400

    created quotes component, remade App component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Quotes from './Components/Quotes'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={'http://clipart-library.com/image_gallery2/Kanye-West-PNG.png'} className="App-logo" alt="logo" />
+        <h1> Yeezy Said... </h1>
+        <img src={'http://clipart-library.com/image_gallery2/Kanye-West-PNG.png'} className="App-logo" alt="logo" />
       </header>
+      <Quotes />
     </div>
   );
 }
---
+++ app/src/Components/Quotes.js
@@ -0,0 +1,9 @@
+function Quotes() {
+    return (
+        <div className='quotes-container'>
+            Info about the quote will go here
+        </div>
+    )
+}
+
+export default Quotes;
