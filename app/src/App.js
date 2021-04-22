e04f27e0951e2f035e7d65c6040f6689b907b858
Justin Peczenij <justinpeczenij@gmail.com>
2021-04-22 16:00:43 -0400

    using local pictures

--- app/src/App.js
+++ app/src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'http://clipart-library.com/image_gallery2/Kanye-West-PNG.png'} className="App-logo" alt="logo" />
+        <img src={'Kanye-West-PNG.png'} className="App-logo" alt="logo" />
         <h1> Yeezy Said... </h1>
-        <img src={'http://clipart-library.com/image_gallery2/Kanye-West-PNG.png'} className="App-logo" alt="logo" />
+        <img src='Kanye-West-PNG.png' className="App-logo" alt="logo" />
       </header>
       <Quotes />
     </div>
